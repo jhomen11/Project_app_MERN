26782d9903f3f09762870781da14f012e2ba8117
Jhonny Mendoza <jhomen11@gmail.com>
2022-02-28 23:07:56 -0300

    validacion para evitar usuarios duplicados

--- Back/controllers/usuarioController.js
+++ Back/controllers/usuarioController.js
@@ -1,6 +1,15 @@
 import Usuario from "../models/Usuario.js"
 
 const registrarUsuario = async (req, res) => {
+
+    //validacion de datos duplicados
+    const { email } = req.body
+    const usuarioExiste = await Usuario.findOne({ email })
+    
+    if(usuarioExiste){
+        const error = new Error("El correo ingresado ya existe en la base de dtos")
+        return res.status(400).json({msg: error.message})
+    }
     
     try {
         //Almacenar información en la BD
