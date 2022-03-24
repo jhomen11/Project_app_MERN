1f9cc889617d5d5fdb0d87c0e09f572536d5b11e
Jhonny Mendoza <jhomen11@gmail.com>
2022-03-24 14:42:13 -0300

    Modelo de tareas

--- Back/models/tarea.js
+++ Back/models/tarea.js
@@ -1,5 +1,38 @@
 import mongoose from "mongoose";
 
 const tareasSchema = mongoose.Schema({
-    
+    nombre: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    descripcion: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    estado: {
+        type: Boolean,
+        default: false
+    },
+    fechaEntrega: {
+        type: Date,
+        required: true,
+        default: Date.now()
+    },
+    prioridad: {
+        type: String,
+        required: true,
+        enum: ["Baja", "Media", "Baja"]
+    },
+    proyecto : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Proyecto'
+    }
+}, {
+    timestamps: true
 })
+
+const Tarea = mongoose.model("Tarea", tareasSchema)
+
+export default Tarea
