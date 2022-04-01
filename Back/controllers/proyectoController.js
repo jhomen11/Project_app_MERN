18c9c702d621dc6bdf7f507a4fca9447859f91ee
Jhonny <jhomen11@gmail.com>
2022-04-01 09:06:35 -0300

    Ruta para editar un proyecto

--- Back/controllers/proyectoController.js
+++ Back/controllers/proyectoController.js
@@ -35,7 +35,34 @@ const obtenerProyecto = async (req, res) =>{
     res.json(proyecto)
 }
 
-const editarProyectos = async (req, res) =>{}
+const editarProyectos = async (req, res) =>{
+    const { id } = req. params
+    
+    //Consultar si el proyecto existe en la bd
+    const proyecto = await Proyecto.findById(id)
+    if(!proyecto){
+        const error = new Error("El Proyecto no Existe")
+        return res.status(404).json({msg: error.message})
+    }
+    //Validando si el id del usuario pertenece al mismo que lo esta consultando
+    if(proyecto.creador.toString() !== req.usuario._id.toString()){
+        const error = new Error("Accion No Permitida")
+        return res.status(401).json({msg: error.message})
+    }
+
+    proyecto.nombre = req.body.nombre || proyecto.nombre
+    proyecto.descripcion = req.body.descripcion || proyecto.descripcion
+    proyecto.fechaEntrega = req.body.fechaEntrega || proyecto.fechaEntrega
+    proyecto.cliente = req.body.cliente || proyecto.cliente
+
+    try {
+        const proyectoEditado = await proyecto.save()
+        res.json(proyectoEditado)  
+
+    } catch (error) {
+        console.log(error)
+    }
+}
 const eliminarProyectos = async (req, res) =>{}
 const agregarColaborador = async (req, res) =>{}
 const eliminarColaborador = async (req, res) =>{}
