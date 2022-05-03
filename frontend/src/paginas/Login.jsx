f0effaacdbe69cb7c2d5eedc7065080211f2f7bd
Jhonny Mendoza <jhomen11@gmail.com>
2022-05-03 13:08:52 -0400

    verificación de token de usuario

--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Alertas from '../components/Alertas'
 import clienteAxios from "../config/clienteAxios";
-
+import useAuth from "../hooks/useAuth";
 
 const Login = () => {
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [alerta, setAlerta] = useState({})
+
+  const { setAuth } = useAuth()
   
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -24,7 +26,8 @@ const Login = () => {
       const { data } = await clienteAxios.post(`usuarios/login`, { email, password})
       setAlerta({})
       localStorage.setItem('token', data.token)
-      
+      setAuth(data)
+
     } catch (error) {
       setAlerta({
         msg: error.response.data.msg ,
