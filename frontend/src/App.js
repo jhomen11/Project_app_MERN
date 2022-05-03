a11185b0ef2fec6c50bcf2a6ed229a6141fe2f40
Jhonny Mendoza <jhomen11@gmail.com>
2022-05-03 11:21:20 -0400

    context de autenticacion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,34 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AuthLayout from './layouts/AuthLayout.jsx'
-import ConfirmarCuenta from './paginas/ConfirmarCuenta.jsx'
-import Login from './paginas/Login.jsx'
-import NuevoPassword from './paginas/NuevoPassword'
-import OlvidoPassword from './paginas/OlvidoPassword'
-import Registrar from './paginas/Registrar'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthLayout from "./layouts/AuthLayout.jsx";
+import ConfirmarCuenta from "./paginas/ConfirmarCuenta.jsx";
+import Login from "./paginas/Login.jsx";
+import NuevoPassword from "./paginas/NuevoPassword";
+import OlvidoPassword from "./paginas/OlvidoPassword";
+import Registrar from "./paginas/Registrar";
 
+import { AuthProvider } from "./context/AuthProvider";
 
 const App = () => {
-  
   return (
     <>
-    
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element= { <AuthLayout/> }>
-          <Route index element= { <Registrar/> }/>
-          <Route path='login' element= { <Login/> }/>
-          <Route path='olvido-password' element= { <OlvidoPassword/> }/>
-          <Route path='olvido-password/:token' element= { <NuevoPassword/> }/>
-          <Route path='confirmar/:id' element= { <ConfirmarCuenta/> }/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<AuthLayout />}>
+              <Route index element={<Registrar />} />
+              <Route path="login" element={<Login />} />
+              <Route path="olvido-password" element={<OlvidoPassword />} />
+              <Route
+                path="olvido-password/:token"
+                element={<NuevoPassword />}
+              />
+              <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+            </Route>
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
